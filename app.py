8ffcd5def82b66a3dc1ff989716df8132fe26342
Julie Vaughn <julie.r.vaughn@gmail.com>
2023-12-21 10:46:53 -0800

    test cloud settings with commented out code

--- app.py
+++ app.py
@@ -24,54 +24,54 @@ option = st.selectbox(
     'Select a GEO methylation dataset to work with',
      data_sources['ID'])
 
-## Data loading functions
-@st.cache_data(persist=True)
-def load_data(name, nrows=None):
-    data_source = DataLibrary().get(name)
-    data=data_source.load()
-    methylation_data = data.dnam
-    metadata = data.metadata
-    return methylation_data, metadata
+# ## Data loading functions
+# @st.cache_data(persist=True)
+# def load_data(name, nrows=None):
+#     data_source = DataLibrary().get(name)
+#     data=data_source.load()
+#     methylation_data = data.dnam
+#     metadata = data.metadata
+#     return methylation_data, metadata
 
 
 
-methylation_data, metadata = load_data(option)
-n = len(methylation_data.columns)
+# methylation_data, metadata = load_data(option)
+# n = len(methylation_data.columns)
 
-st.write(f"{option} contains methylation data from {n} subjects.")
-st.write(f"Metadata available for {option}: {metadata.columns.values}")
-
-
-## Add epigenetic clock plots
-
-@st.cache_data(persist=True)
-def load_predictions():
-    horvath_results = gallery.get("Horvathv1").predict(methylation_data)
-    hannum_results = gallery.get("Hannum").predict(methylation_data)
-    phenoage_results = gallery.get("PhenoAge").predict(methylation_data)
-    actual_age = metadata['age']
-    plot_data = pd.DataFrame({
-    'Horvath 2013': horvath_results,
-    'Hannum 2013': hannum_results,
-    'PhenoAge 2018': phenoage_results,
-    "Actual Age": actual_age})
-    #TODO add more plots / make them checkboxes to select for
-    return plot_data
-
-gallery = ModelGallery()
-with st.spinner("Please wait while clock models load..."):
-    results_data = load_predictions()
+# st.write(f"{option} contains methylation data from {n} subjects.")
+# st.write(f"Metadata available for {option}: {metadata.columns.values}")
 
 
-plot_data = results_data
-plot_data.index=plot_data['Actual Age']
+# ## Add epigenetic clock plots
 
-relplot = sns.relplot(data=plot_data, kind="scatter", s=15)
+# @st.cache_data(persist=True)
+# def load_predictions():
+#     horvath_results = gallery.get("Horvathv1").predict(methylation_data)
+#     hannum_results = gallery.get("Hannum").predict(methylation_data)
+#     phenoage_results = gallery.get("PhenoAge").predict(methylation_data)
+#     actual_age = metadata['age']
+#     plot_data = pd.DataFrame({
+#     'Horvath 2013': horvath_results,
+#     'Hannum 2013': hannum_results,
+#     'PhenoAge 2018': phenoage_results,
+#     "Actual Age": actual_age})
+#     #TODO add more plots / make them checkboxes to select for
+#     return plot_data
 
-relplot.set_axis_labels("Actual Age", "Predicted Age")
+# gallery = ModelGallery()
+# with st.spinner("Please wait while clock models load..."):
+#     results_data = load_predictions()
 
-st.write(f"Here are a few epigenetic clocks on {option}")
-st.pyplot(relplot)
+
+# plot_data = results_data
+# plot_data.index=plot_data['Actual Age']
+
+# relplot = sns.relplot(data=plot_data, kind="scatter", s=15)
+
+# relplot.set_axis_labels("Actual Age", "Predicted Age")
+
+# st.write(f"Here are a few epigenetic clocks on {option}")
+# st.pyplot(relplot)
 
 
 
